ce91b72196d77b0d82753e507dd9d271b38570df
Alessandro Jean <alessandrojean@gmail.com>
2019-05-05 17:00:08 -0300

    Add flag to export to json file.

--- src/commands/export.js
+++ src/commands/export.js
@@ -7,28 +7,49 @@ const {defaultOutput} = require('../config/exporter')
 
 const writeFilePromise = promisify(writeFile)
 
+const createM3U8 = channels => {
+  let fileContent = '#EXTM3U\n'
+  for (let tv of channels) {
+    const tvgName = tv.tvgName ? `tvg-name="${tv.tvgName}" ` : ''
+    const aspectRatio = tv.aspectRatio ?
+      `aspect-ratio="${tv.aspectRatio}" ` :
+      ''
+
+    fileContent += `\n#EXTINF:-1 ${tvgName}tvg-logo="${
+      tv.tvgLogo
+    }" ${aspectRatio}type="stream", ${tv.name}`
+    fileContent += `\n#EXTBG: ${tv.color}`
+    fileContent += `\n${tv.streamUrl}\n`
+  }
+  return fileContent
+}
+
+const createJson = channels => {
+  const parsed = channels.map(channel => ({
+    name: channel.name,
+    aspectRatio: channel.aspectRatio,
+    color: channel.color,
+    continent: channel.continent,
+    country: channel.country,
+    logo: channel.tvgLogo,
+    url: channel.streamUrl,
+  }))
+
+  return JSON.stringify(parsed, null, 2)
+}
+
 class ExportCommand extends Command {
   async run() {
     const {flags} = this.parse(ExportCommand)
-    const output = flags.output ? path.join(process.cwd(), flags.output) : defaultOutput
+    const output = flags.output ?
+      path.join(process.cwd(), flags.output) :
+      defaultOutput + (flags.json ? '.json' : '.m3u8')
 
     const channels = await Channel.findAll({
-      order: [
-        ['continent', 'ASC'],
-        ['country', 'ASC'],
-        ['name', 'ASC'],
-      ],
+      order: [['continent', 'ASC'], ['country', 'ASC'], ['name', 'ASC']],
     })
 
-    let fileContent = '#EXTM3U\n'
-    for (let tv of channels) {
-      const tvgName = tv.tvgName ? `tvg-name="${tv.tvgName}" ` : ''
-      const aspectRatio = tv.aspectRatio ? `aspect-ratio="${tv.aspectRatio}" ` : ''
-
-      fileContent += `\n#EXTINF:-1 ${tvgName}tvg-logo="${tv.tvgLogo}" ${aspectRatio}type="stream", ${tv.name}`
-      fileContent += `\n#EXTBG: ${tv.color}`
-      fileContent += `\n${tv.streamUrl}\n`
-    }
+    const fileContent = flags.json ? createJson(channels) : createM3U8(channels)
 
     await writeFilePromise(output, fileContent)
     this.log(`Exported list with success in ${output}`)
@@ -39,6 +60,7 @@ ExportCommand.description = 'Export the database channels to a M3U8 file.'
 
 ExportCommand.flags = {
   output: flags.string({char: 'o', description: 'non-default output file'}),
+  json: flags.boolean({char: 'j', description: 'exports in json format'}),
 }
 
 module.exports = ExportCommand
--- src/config/exporter.js
+++ src/config/exporter.js
@@ -1,5 +1,5 @@
 const path = require('path')
 
 module.exports = {
-  defaultOutput: path.resolve(__dirname, '../../docs/list.m3u8'),
+  defaultOutput: path.resolve(__dirname, '../../docs/list'),
 }
