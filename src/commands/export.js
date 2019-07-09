3ad28fe3057429c2a4f0d6eff8650cb501d29514
Alessandro Jean <alessandrojean@gmail.com>
2019-07-08 22:10:21 -0300

    Group channels by country.

--- src/commands/export.js
+++ src/commands/export.js
@@ -3,9 +3,14 @@ const {promisify} = require('util')
 const {writeFile} = require('fs')
 const path = require('path')
 const {Channel} = require('../models')
-const {defaultOutput} = require('../config/exporter')
+const {defaultOutput, defaultPath} = require('../config/exporter')
+const {countries} = require('countries-list')
 
 const writeFilePromise = promisify(writeFile)
+
+const getCountryCode = country => Object
+  .keys(countries)
+  .filter(key => countries[key].name === country)
 
 const createM3U8 = channels => {
   let fileContent = '#EXTM3U\n'
@@ -20,6 +25,18 @@ const createM3U8 = channels => {
     }" ${aspectRatio}type="stream", ${tv.name}`
     fileContent += `\n#EXTBG: ${tv.color}`
     fileContent += `\n${tv.streamUrl}\n`
+  }
+  return fileContent
+}
+
+const createMasterM3U8 = dbCountries => {
+  let fileContent = '#EXTM3U\n'
+  for (let country of dbCountries) {
+    const countryCode = getCountryCode(country.country)
+    const flag = `https://www.countryflags.io/${countryCode}/flat/64.png`
+
+    fileContent += `\n#EXTINF:0 type="playlist" tvg-logo="${flag}", ${country.country}`
+    fileContent += `\n${countryCode}.m3u8\n`
   }
   return fileContent
 }
@@ -41,15 +58,43 @@ const createJson = channels => {
 class ExportCommand extends Command {
   async run() {
     const {flags} = this.parse(ExportCommand)
-    const output = flags.output ?
-      path.join(process.cwd(), flags.output) :
-      defaultOutput + (flags.json ? '.json' : '.m3u8')
+    const output = defaultOutput + (flags.json ? '.json' : '.m3u8')
 
-    const channels = await Channel.findAll({
-      order: [['continent', 'ASC'], ['country', 'ASC'], ['name', 'ASC']],
+    if (flags.json) {
+      const channels = await Channel.findAll({
+        order: [['continent', 'ASC'], ['country', 'ASC'], ['name', 'ASC']],
+      })
+
+      await writeFilePromise(createJson(channels))
+
+      this.log(`Exported list with success in ${output}`)
+      return
+    }
+
+    const dbCountries = await Channel.findAll({
+      attributes: ['country', 'continent'],
+      group: ['country'],
+      order: [['country', 'ASC']],
     })
 
-    const fileContent = flags.json ? createJson(channels) : createM3U8(channels)
+    for (let country of dbCountries) {
+      /* eslint-disable no-await-in-loop */
+      const countryChannels = await Channel.findAll({
+        order: [['country', 'ASC'], ['name', 'ASC']],
+        where: {
+          country: country.country,
+        },
+      })
+
+      const countryCode = getCountryCode(country.country)
+
+      const fileContent = createM3U8(countryChannels)
+      const fileName = path.join(defaultPath, 'countries', countryCode[0] + '.m3u8')
+
+      await writeFilePromise(fileName, fileContent)
+    }
+
+    const fileContent = createMasterM3U8(dbCountries)
 
     await writeFilePromise(output, fileContent)
     this.log(`Exported list with success in ${output}`)
@@ -59,7 +104,6 @@ class ExportCommand extends Command {
 ExportCommand.description = 'Export the database channels to a M3U8 file.'
 
 ExportCommand.flags = {
-  output: flags.string({char: 'o', description: 'non-default output file'}),
   json: flags.boolean({char: 'j', description: 'exports in json format'}),
 }
 
--- src/config/exporter.js
+++ src/config/exporter.js
@@ -1,5 +1,6 @@
 const path = require('path')
 
 module.exports = {
+  defaultPath: path.resolve(__dirname, '../../docs'),
   defaultOutput: path.resolve(__dirname, '../../docs/list'),
 }
