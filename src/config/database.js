14d0d4821d30966ec0359390260da4621ed405cd
Alessandro Jean <alessandrojean@gmail.com>
2019-11-16 14:47:25 -0300

    Start gh-pages branch.

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  dialect: 'sqlite',
-  storage: path.resolve(__dirname, '../database/database.sqlite'),
-  logging: false,
-}
--- src/config/exporter.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  baseUrl: 'https://alessandrojean.github.io/m3u8-cli/',
-  defaultPath: path.resolve(__dirname, '../../docs'),
-  defaultOutput: path.resolve(__dirname, '../../docs/list'),
-}
